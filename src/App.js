55799e5328251663a8950f67c16a61796dd26bf4
makaraDevIt <makarasam68@gmail.com>
2025-01-15 19:33:49 +0700

    installing_boostrap_css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { CiSearch } from "react-icons/ci";
-
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Button,Container, Col,Row} from 'react-bootstrap';
+ 
 function App() {
   return (
     <div className="App">
+        <Container>
+        <Row>
+          <Col md={3}>
+            <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello, bootstrap</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello, bootstrap</h3>
+          </Col>
+        </Row>
+        </Container> 
+
         <h1>Find me right now <CiSearch/></h1>
+        <Button>Hello</Button>
     </div>
   );
 }
